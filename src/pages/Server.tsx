f49e97c64887db413b153faa56a3cfd91f51856b
Gregoire-F <gregoire.frou@gmail.com>
2026-05-29 11:19:35 +0200

    FIX: j'avais dupliqué un btn supprimer

--- src/pages/Server.tsx
+++ src/pages/Server.tsx
@@ -140,14 +140,6 @@ export default function Server() {
                 Supprimer
               </button>
             )}
-            {canDelete(server) && (
-              <button
-                onClick={() => handleDelete(server.id)}
-                className="px-4 py-2 bg-red-500 text-white border-0 rounded cursor-pointer"
-              >
-                Supprimer
-              </button>
-            )}
           </li>
         ))}
       </ul>
