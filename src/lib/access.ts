b3467b6d24b63e9d29f52b2ca15109756c4eb947
Gregoire-F <gregoire.frou@gmail.com>
2026-05-29 11:17:00 +0200

    Ajout de commentaires

--- src/lib/access.ts
+++ src/lib/access.ts
@@ -20,21 +20,31 @@ export function useCanEdit (entityType:string){
     const role = user?.user.roles[0]
 
     return (entity:any) => {
+        // Les users ne peuvent rien éditer
         if (role === "ROLE_USER") return false
+
+        // Le SUPER_ADMIN peut tout éditer
         if (role === "ROLE_SUPER_ADMIN") return true
+
+
+        // Serveurs et Vm : seul SUPER_ADMIN a le droit d'éditer
         if (entityType === "server" || entityType === "vm"){
             return role === "ROLE_SUPER_ADMIN"
         }
+
+        // CLIENT_ADMIN ne peut éditer que les users de sa propre équipe
         if (role === "ROLE_CLIENT_ADMIN"){
             if (entityType === "user"){
                 return entity?.team?.id === user?.user?.team?.id;
             }
             return false
         }
+
+        // ROLE_DEVELOPER : peut éditer serveurs et VMs, pas les users
         if (role === "ROLE_DEVELOPER"){
             if (entityType === "server" || entityType === "vm") return true;
             return false;
         }
-        // return false
+        return false
     }
 }
