313d290491d7c1b2d98fb9e968a6f39c6bcff442
Gregoire-F <gregoire.frou@gmail.com>
2026-05-29 16:12:42 +0200

    Style différent dans Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    setError("Ce ne sont pas les bons identifiants !");
+    setError("Erreur dans les identifiants !");
 
     try {
       const res = await fetch(`${import.meta.env.VITE_API_URL}login`, {
@@ -31,12 +31,15 @@ export default function Login() {
   return (
     <>
       <main className="relative bg-slate-50 dark:bg-slate-950 h-svh flex flex-col justify-center items-center">
-        <h1>Bienvenue sur Badger</h1>
         <div className="px-14 md:px-24 py-14 rounded border border-slate-300 bg-white dark:bg-slate-900 items-center justify-center flex flex-row gap-16">
-          <h2 className="font-semibold">Connectez-vous</h2>
+          <h1 className="font-semibold text-xl">Bienvenue sur Badger</h1>
           <form onSubmit={handleSubmit} className="grid gap-4">
+            <h2 className="font-bold text-amber-500 border-b border-amber-500">
+              Se connecter
+            </h2>
             {error && <p style={{ color: "red" }}>{error}</p>}
             <input
+              className="border-2 rounded outline-amber-500"
               type="email"
               placeholder="Email"
               value={email}
@@ -44,13 +47,19 @@ export default function Login() {
               required
             />
             <input
+              className="border-2 rounded outline-amber-500"
               type="password"
               placeholder="Mot de passe"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <button type="submit" className="py-2 px-4 bg-black text-white rounded-lg cursor-pointer">Se connecter</button>
+            <button
+              type="submit"
+              className="py-2 px-4 bg-black text-white rounded-lg cursor-pointer"
+            >
+              Se connecter
+            </button>
           </form>
         </div>
       </main>
